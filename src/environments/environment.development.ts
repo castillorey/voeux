5a4e5e33b415bf96555ac809f7ac910e83d573f5
Kevin Castillo <castilloreyeskm@gmail.com>
2024-01-09 18:14:06 -0500

    Initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+	production: false,
+	appname:'voeux',
+	supabase: {
+		url: 'https://ehkhgvrervvvipphnxoq.supabase.co',
+		key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoa2hndnJlcnZ2dmlwcGhueG9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4MjA0ODMsImV4cCI6MjAyMDM5NjQ4M30.9nyERvjeMP1kxXQvk1Evf12VfgkNhUzBPYA690WgibY'
+	},
+
+};
